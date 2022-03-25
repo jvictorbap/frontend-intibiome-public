6c4927005e24937b8f9221bf20907e4d4194104b
Victor Santiago <hello@victordeveloper.com.br>
2022-03-25 14:34:50 -0300

    feat: mobile first - import header

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import logo from '../images/Module 1 - Header 970x60025.png'
+import lupa from '../images/ic.search.svg'
+
+function Header() {
+  return (
+    <header className=''>
+      <nav className='flex w-full justify-around items-center shadow-md'>
+        <ul>
+          <li>
+            <a href="">
+              <AiOutlineMenu />
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="">
+              <img src={logo} className="w-[143px] h-[58px]" alt="" />
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="">
+              <img src={lupa} alt="" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header

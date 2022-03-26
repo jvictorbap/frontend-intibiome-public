812ee6bf322e84f2dec72712dd1a5f657d15bb74
Victor Santiago <hello@victordeveloper.com.br>
2022-03-25 21:28:24 -0300

    feat: mobile first - import products section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/Banner';
 import bannerUp from './images/banner-intibiome-02@2x.png';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
+import Products from './components/Products';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner banner={bannerUp} />
       <Section1 />
       <Section2 />
+      <Products />
     </div>
   );
 }

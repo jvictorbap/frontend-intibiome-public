4d7538211bca8378ca2ec7a77492c73c530ab5d0
Victor Santiago <hello@victordeveloper.com.br>
2022-03-26 16:24:58 -0300

    feat: mobile first - import header hovers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,227 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import logo from '../images/Module 1 - Header 970x60025.png'
 import lupa from '../images/ic.search.svg'
 import HeaderNavbarUl from './HeaderNavbarUl'
+import { RiCloseLine } from 'react-icons/ri'
+import { IoIosArrowDown } from 'react-icons/io'
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = React.useState(false)
+  const [aboutOpen, setAboutOpen] = React.useState(false)
+  const [productsOpen, setProductsOpen] = React.useState(false)
+  const [intimateOpen, setIntimateOpen] = React.useState(false)
+
+  function handleClick() {
+    setMenuOpen(!menuOpen)
+  }
+
+  function hamburguer() {
+    return (
+      <div className='absolute z-10 bg-blue w-full h-screen font-semibold text-white tracking-[.32px]'>
+        <nav className='mt-10 w-[119px] h-[705px] ml-4'>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>brand philosophy</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>product technology</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>all products</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>intibiome wellness</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>intibiome active</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>intibiome agecare</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>article 1</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>article 2</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>article 3</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>faq</a>
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li className='w-[181px] p-2 h-[47px]'>
+              <button className='font-semibold'>
+                <a className=''>contact us</a>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
+  function aboutHandler() {
+    setAboutOpen(!aboutOpen)
+  }
+
+  function aboutRender() {
+    return (
+      <div className='absolute z-10 flex w-full h-[49px] justify-center bg-brandGray'>
+        <nav className='h-[49px] flex'>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>brand philosophy</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>product technology</a>
+              </li>
+            </button>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
+  function productHandler() {
+    setProductsOpen(!productsOpen)
+  }
+
+  function productRender() {
+    return (
+      <div className='absolute z-10 flex w-full h-[49px] justify-center bg-brandGray'>
+        <nav className='h-[49px] flex'>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>all products</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>intibiome wellness</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>intibiome active</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>intibiome agecare</a>
+              </li>
+            </button>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
+  function intimateHandler() {
+    setIntimateOpen(!intimateOpen)
+  }
+
+  function intimateRender() {
+    return (
+      <div className='absolute z-10 flex w-full h-[49px] justify-center bg-brandGray'>
+        <nav className='h-[49px] flex'>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>article 1</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>article 2</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>article 3</a>
+              </li>
+            </button>
+          </ul>
+          <ul>
+            <button className='w-[181px] h-[47px]'>
+              <li>
+                <a href="" className='text-white font-medium'>faq</a>
+              </li>
+            </button>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
   return (
     <header className=''>
-      <nav className='flex w-full justify-around lg:justify-evenly items-center shadow-md'>
+      <nav nav className='flex w-full justify-around lg:justify-evenly items-center shadow-md' >
         <ul className='md:invisible'>
           <li>
-            <a href="">
-              <AiOutlineMenu />
-            </a>
+            <button id='hamburguer' onClick={() => handleClick()}>
+              {menuOpen ? <RiCloseLine /> : <AiOutlineMenu />}
+            </button>
           </li>
         </ul>
         <ul>
@@ -30,16 +238,36 @@ function Header() {
             </a>
           </li>
         </ul>
-      </nav>
-      <div className='h-[49px] w-full bg-blue items-center hidden md:flex justify-center'>
+      </nav >
+      {menuOpen ? hamburguer() : null}
+      <div id='navbar' className='h-[49px] w-full bg-blue items-center hidden md:flex justify-center'>
         <nav className='flex justify-center h-full items-center'>
-          <HeaderNavbarUl text="about us" icone='true' />
-          <HeaderNavbarUl text="our products" icone='true' />
-          <HeaderNavbarUl text="intimate health" icone='true' />
-          <HeaderNavbarUl text="contact us" />
+          <ul className='w-[190px] h-[48px] flex items-center'>
+            <li className='w-full'>
+              <a href="#0" onMouseEnter={() => aboutHandler()} onMouseOut={() => aboutHandler()} className='text-white flex items-center font-medium gap-4 justify-center text-lg '>about us <IoIosArrowDown /></a>
+            </li>
+          </ul>
+          <ul className='w-[190px] h-[48px] flex items-center'>
+            <li className='w-full'>
+              <a href="#0" onMouseEnter={() => productHandler()} onMouseOut={() => productHandler()} className='text-white flex items-center font-medium gap-4 justify-center text-lg '>our products <IoIosArrowDown /></a>
+            </li>
+          </ul>
+          <ul id='intimate' className='w-[190px] h-[48px] flex items-center'>
+            <li className='w-full'>
+              <a href="#0" onMouseEnter={() => intimateHandler()} onMouseOut={() => intimateHandler()} className='text-white flex items-center font-medium gap-4 justify-center text-lg '>intimate health <IoIosArrowDown /></a>
+            </li>
+          </ul>
+          <ul id='intimate' className='w-[190px] h-[48px] flex items-center'>
+            <li className='w-full'>
+              <a href="#0" className='text-white flex items-center font-medium gap-4 justify-center text-lg '>contact us</a>
+            </li>
+          </ul>
         </nav>
       </div>
-    </header>
+      {aboutOpen ? aboutRender() : null}
+      {productsOpen ? productRender() : null}
+      {intimateOpen ? intimateRender() : null}
+    </header >
   )
 }
 

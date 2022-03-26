62ffb98c77ca9dd3930890e0f23da521cb4fd247
Victor Santiago <hello@victordeveloper.com.br>
2022-03-25 21:29:31 -0300

    feat: mobile first - import another banner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import bannerUp from './images/banner-intibiome-02@2x.png';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Products from './components/Products';
+import bannerDown from './images/banner-intibiome-05.png';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Section1 />
       <Section2 />
       <Products />
+      <Banner banner={bannerDown} mt="mt-16" />
     </div>
   );
 }
